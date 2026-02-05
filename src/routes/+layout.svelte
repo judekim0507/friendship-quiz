90da4964973e006f34bf46ebe0883dddff3177bc
Jude Kim <75868541+judekim0507@users.noreply.github.com>
2026-02-05 11:17:38 -0800

    feat: add title for SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,9 @@
     let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head
+    ><link rel="icon" href={favicon} /><title
+        >Cosmic Twins - Burnaby North Secondary School Student Government</title
+    ></svelte:head
+>
 {@render children()}
